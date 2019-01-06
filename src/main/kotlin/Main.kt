edf3f1dcb98f3e216d46309e75696e11c2f81128
Seng Wee <swong039@e.ntu.edu.sg>
2019-01-06 16:40:04 +0800

    String concatentation and String interpolation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,13 +2,17 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
-    var wee: String
-    wee = "This is Seng Wee"
-    println(wee)
-    println("")
+    var sw: String
+    sw = "Seng Wee"
+    println(sw)
+    println()
 
     var salary: Int = 100000
     var annualSalary: Int = salary * 12
-    println("My annual salary is $" + annualSalary + ".")
+    println("My annual salary is $" + annualSalary + ".") //String concatenation
 
+    println("My name is $sw.") //String interpolation
+
+    var daysInWeek : Int = 7
+    println("Number of week(s) in $daysInWeek days is ${daysInWeek / 7}.")
 }
