b92502d57cdbb244f74b2b18482a2e7fed549315
Seng Wee <swong039@e.ntu.edu.sg>
2019-01-06 16:54:02 +0800

    val vs var

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,17 @@ fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
 
-    var sw: String
+    val sw: String
     sw = "Seng Wee"
     println(sw)
     println()
+    // val vs var - val variables are immutable while var variables are mutable.
 
     var salary: Int = 100000
     var annualSalary: Int = salary * 12
     println("My annual salary is $" + annualSalary + ".") //String concatenation
 
-    println("My name is $sw.") //String interpolation
+    println("My name is $sw.") // String interpolation
 
     var daysInWeek : Int = 7
     println("Number of week(s) in $daysInWeek days is ${daysInWeek / 7}.")
