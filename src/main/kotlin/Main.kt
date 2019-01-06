e45fb46da9419caff24d8ae53c37575980e4dad3
Seng Wee <swong039@e.ntu.edu.sg>
2019-01-06 18:13:33 +0800

    Conditional Statements

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,58 @@
 fun main(args: Array<String>) {
-    println("Hello World")
-    println("My first Kotlin program")
+    /*
+    Part 2
+    1. Conditions
+    */
+//    val lives = 3
+//    var isGameOver = lives < 1
+//
+//    if (isGameOver) {
+//        println("Game Over!")
+//    } else {
+//        println("Stil ALIVE!")
+//    }
 
-    val sw: String
-    sw = "Seng Wee"
-    println(sw)
-    println()
-    // val vs var - val variables are immutable while var variables are mutable.
+    println("How old are you: ")
+    val age = readLine()!!.toInt()
+    println("Your age is $age.")
 
-    var salary: Int = 100000
-    var annualSalary: Int = salary * 12
-    println("My annual salary is $" + annualSalary + ".") //String concatenation
+    val message: String
+    message = when {
+        age < 21 -> "You are too young to vote!"
+        age >= 100 -> "Congratulations! You are a centenarian, you are eligible to vote!"
+        else -> "You are eligible to vote in the next General Elections!"
+    }
 
-    println("My name is $sw.") // String interpolation
+//    message = if (age < 21) {
+//        "You are too young to vote!"
+//    } else if (age >= 100) {
+//        "Congratulations! You are a centenarian, you are eligible to vote!"
+//    } else {
+//        "You are eligible to vote in the next General Elections!"
+//    }
 
-    var daysInWeek : Int = 7
-    println("Number of week(s) in $daysInWeek days is ${daysInWeek / 7}.")
+    println(message)
+    /*
+    Part 1
+    1. Variables and Types
+    2. String Concatenation & String Interpolation
+    3. val vs var
+    */
+//    println("Hello World")
+//    println("My first Kotlin program")
+//
+//    val sw: String
+//    sw = "Seng Wee"
+//    println(sw)
+//    println()
+//    // val vs var - val variables are immutable while var variables are mutable.
+//
+//    var salary: Int = 100000
+//    var annualSalary: Int = salary * 12
+//    println("My annual salary is $" + annualSalary + ".") //String concatenation
+//
+//    println("My name is $sw.") // String interpolation
+//
+//    var daysInWeek : Int = 7
+//    println("Number of week(s) in $daysInWeek days is ${daysInWeek / 7}.")
 }
