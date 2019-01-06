e1c8aa20ab5f95f45df3afdf3faf97094e207ad9
Seng Wee <swong039@e.ntu.edu.sg>
2019-01-06 16:10:22 +0800

    Variables and Types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
 fun main(args: Array<String>) {
     println("Hello World")
     println("My first Kotlin program")
+
+    var wee: String
+    wee = "This is Seng Wee"
+    println(wee)
+    println("")
+
+    var salary: Int = 100000
+    var annualSalary: Int = salary * 12
+    println("My annual salary is $" + annualSalary + ".")
+
 }
